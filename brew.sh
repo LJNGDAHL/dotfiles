4e9de0cc6f87e685bfff7fbaa54eafcdad6c3b06
Katarina Ljungdahl <katarina@earthpeople.se>
2019-10-20 11:09:10 +0200

    Update settings

--- brew.sh
+++ brew.sh
@@ -13,28 +13,22 @@ brew tap caskroom/versions
 # Install native apps
 brew cask install 1password 2> /dev/null
 brew cask install caprine 2> /dev/null
-brew cask install dropbox 2> /dev/null
 brew cask install docker 2> /dev/null
 brew cask install figma 2> /dev/null
 brew cask install filezilla 2> /dev/null
 brew cask install firefox 2> /dev/null
 brew cask install flux 2> /dev/null
 brew cask install google-chrome 2> /dev/null
-brew cask install google-chrome-canary 2> /dev/null
-brew cask install google-drive 2> /dev/null
-brew cask install hyper 2> /dev/null
-brew cask install insomnia 2> /dev/null
+brew cask install google-chrome-canary 2> /dev/null 
 brew cask install mamp 2> /dev/null
-brew cask install transmission 2> /dev/null
 brew cask install virtualbox 2> /dev/null
-brew cask install opera 2> /dev/null
-brew cask install opera-developer 2> /dev/null
 brew cask install postman 2> /dev/null
+brew cask install react-native-debugger 2> /dev/null
 brew cask install silverlight 2> /dev/null
-brew cask install sketch 2> /dev/null
 brew cask install sourcetree 2> /dev/null
 brew cask install spotify 2> /dev/null
 brew cask install slack 2> /dev/null
+brew cask install spark 2> /dev/null
 brew cask install visual-studio-code 2> /dev/null
 
 # Install fonts
